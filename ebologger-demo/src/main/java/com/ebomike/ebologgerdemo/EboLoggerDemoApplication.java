f79e9a0cbea162fe9c16f5681e4915a003b4e90e
Ebo <ebo@ebomike.com>
2017-10-20 23:03:44 -0700

    Added example of using an ASCII log sender.

--- ebologger-demo/src/main/java/com/ebomike/ebologgerdemo/EboLoggerDemoApplication.java
+++ ebologger-demo/src/main/java/com/ebomike/ebologgerdemo/EboLoggerDemoApplication.java
@@ -2,9 +2,12 @@ package com.ebomike.ebologgerdemo;
 
 import android.app.Application;
 
+import com.ebomike.ebologger.AsciiLogSender;
 import com.ebomike.ebologger.GlobalConfig;
 import com.ebomike.ebologger.android.ActivityTracker;
 import com.ebomike.ebologger.android.CrashHandler;
+
+import java.io.IOException;
 
 public class EboLoggerDemoApplication extends Application {
     @Override
@@ -18,10 +21,17 @@ public class EboLoggerDemoApplication extends Application {
         // Creating a global configuration is completely optional. We're doing it here for
         // demonstration purposes. By default, we have an Android log writer (all environments),
         // and a stream writer to send data to the UI (dev only).
-        new GlobalConfig.Builder()
-//                .addSender()
-                .build()
-                .activate();
+        try {
+            new GlobalConfig.Builder()
+                    .addSender(new AsciiLogSender.Builder()
+                            .filename("ebologger-demo.log")
+                            .build())
+                    .build()
+                    .activate();
+        } catch (IOException e) {
+            // Possible if we don't have write access in the current directory. Quite likely even,
+            // but doesn't matter in the context of this demonstration.
+        }
 
         // Initializing the crash handler will sends a log to the UI if the application crashes
         // entirely.
