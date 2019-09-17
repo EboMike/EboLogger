8927522152ae3ee12ae35faf085c40420477f4d2
Ebo <ebo@ebomike.com>
2019-09-16 21:42:37 -0700

    Migration to AndroidX.

--- ebologger-demo/src/main/java/com/ebomike/ebologgerdemo/EboLoggerDemo.java
+++ ebologger-demo/src/main/java/com/ebomike/ebologgerdemo/EboLoggerDemo.java
@@ -1,7 +1,7 @@
 package com.ebomike.ebologgerdemo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
