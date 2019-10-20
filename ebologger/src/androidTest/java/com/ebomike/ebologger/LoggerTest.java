7061bf9ce960e462f5b0b443ae14bd9e68a2d68a
Ebo <ebo@ebomike.com>
2019-10-20 00:02:39 -0700

    More AndroidX migration fixes.

--- ebologger/src/androidTest/java/com/ebomike/ebologger/LoggerTest.java
+++ ebologger/src/androidTest/java/com/ebomike/ebologger/LoggerTest.java
@@ -1,12 +1,12 @@
 package com.ebomike.ebologger;
 
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(AndroidJUnit4.class)
 public class LoggerTest {
