d90d468c4631ba5d39a93a0b3ec64aece582a109
ebo <ebo@ebomike.com>
2022-02-05 19:41:41 -0800

    Default logging level in prod is WARNING.

--- ebologger/src/prod/java/com/ebomike/ebologger/FlavorSetup.java
+++ ebologger/src/prod/java/com/ebomike/ebologger/FlavorSetup.java
@@ -6,7 +6,7 @@ import com.ebomike.ebologger.EboLogger.LogLevel;
  * This class has prod-specific values and objects.
  */
 public class FlavorSetup {
-    public static final LogLevel DEFAULT_MIN_LEVEL_LOCAL = LogLevel.INFO;
+    public static final LogLevel DEFAULT_MIN_LEVEL_LOCAL = LogLevel.WARNING;
 
     // This is moot, we don't do remote logging in prod.
     public static final LogLevel DEFAULT_MIN_LEVEL_REMOTE = LogLevel.WTF;
