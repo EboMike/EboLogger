97c28416d5f72d1934dc34c10a5e55e4cc821c86
ebo <ebo@ebomike.com>
2022-02-05 19:42:54 -0800

    Return the dummy log message implementation instead of null in the NullLogger implementation.

--- ebologger/src/main/java/com/ebomike/ebologger/NullLogger.java
+++ ebologger/src/main/java/com/ebomike/ebologger/NullLogger.java
@@ -1,14 +1,17 @@
 package com.ebomike.ebologger;
 
+import com.ebomike.ebologger.model.DummyLogMessage;
 import com.ebomike.ebologger.model.LogMessage;
 
-class NullLogger extends EboLogger {    public NullLogger() {
+/** Implementation of {@link EboLogger} that does nothing. */
+class NullLogger extends EboLogger {
+    public NullLogger() {
         super(Config.getBaseConfig());
     }
 
     @Override
     protected LogMessage createLogMessage(LogLevel severity) {
-        return null;
+        return DummyLogMessage.DUMMY_LOG_MESSAGE;
     }
 
     @Override
