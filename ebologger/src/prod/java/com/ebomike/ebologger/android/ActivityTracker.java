3702ed479f6f9b9e254fcaf02b967d857110e433
Ebo <ebo@ebomike.com>
2026-09-20 21:49:07 -0700

    Delete code to support old APIs

--- ebologger/src/prod/java/com/ebomike/ebologger/android/ActivityTracker.java
+++ ebologger/src/prod/java/com/ebomike/ebologger/android/ActivityTracker.java
@@ -1,11 +1,7 @@
 package com.ebomike.ebologger.android;
 
 import android.app.Application;
-import android.os.Build;
 
-import androidx.annotation.RequiresApi;
-
-@RequiresApi(api = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public class ActivityTracker {
     private static ActivityTracker dummy = new ActivityTracker();
 
